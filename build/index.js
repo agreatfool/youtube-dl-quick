7f07f3b2d9987e7735066742d0764ed85880f496
Jonathan Dai <nightghost5078@gmail.com>
2019-02-12 18:03:09 +0800

    Save.

--- build/index.js
+++ build/index.js
@@ -10,6 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const LibFs = require("mz/fs");
+const LibPath = require("path");
 const LibOs = require("os");
 const program = require("commander");
 const shell = require("shelljs");
@@ -21,7 +22,37 @@ const DEFAULT_OUTPUT_NAME_LIST = `%(playlist)s/${DEFAULT_OUTPUT_NAME_SINGLE}`;
 const DEFAULT_VERTICAL_RESOLUTION = '720';
 const DEFAULT_SOCKS5_HOST = '127.0.0.1';
 const DEFAULT_SOCKS5_PORT = '1086';
-const VALID_VERTICAL_RESOLUTION = ['720', '1080'];
+/**
+ * 140          m4a        audio only DASH audio  130k , m4a_dash container, mp4a.40.2@128k
+ * 136          mp4        1280x720   720p 2338k , avc1.4d401f, 30fps, video only
+ * 298          mp4        1280x720   720p60 3496k , avc1.4d4020, 60fps, video only
+ * 137          mp4        1920x1080  1080p 4366k , avc1.640028, 30fps, video only
+ * 299          mp4        1920x1080  1080p60 5811k , avc1.64002a, 60fps, video only
+ * 271          webm       2560x1440  1440p 8995k , vp9, 30fps, video only
+ * 308          webm       2560x1440  1440p60 13375k , vp9, 60fps, video only
+ * 313          webm       3840x2160  2160p 18369k , vp9, 30fps, video only
+ * 315          webm       3840x2160  2160p60 26725k , vp9, 60fps, video only
+ */
+const VALID_VERTICAL_RESOLUTION = ['720', '1080', '1440', '2160'];
+const DEFAULT_FORMAT = {
+    '720': {
+        '30': '136+140',
+        '60': '298+140'
+    },
+    '1080': {
+        '30': '137+140',
+        '60': '299+140'
+    },
+    '1440': {
+        '30': '271+140',
+        '60': '308+140'
+    },
+    '2160': {
+        '30': '313+140',
+        '60': '315+140'
+    }
+};
+const ERROR_UNAVAILABLE_FORMAT = 'ERROR: requested format not available';
 let IS_SOURCE_LISTFILE = false;
 let IS_SOURCE_PLAYLIST = false;
 program.version(pkg.version)
@@ -33,23 +64,23 @@ program.version(pkg.version)
     '\tvideo in list: "%(playlist)s/[%(uploader)s] %(title)s.%(ext)s"')
     .option('-f, --format <string>', 'download format, default is: \n' +
     '\t-V 720: "298+140" if 60fps available, "136+140" if not\n' +
-    '\t-V 1080: "299+140" if 60fps available, "137+140" if not')
-    .option('-F, --list-formats', 'same as youtube-dl -F, list all available formats of video')
-    .option('-V, --vertical-resolution <number>', 'vertical resolution, default is "720"')
+    '\t-V 1080: "299+140" if 60fps available, "137+140" if not\n' +
+    '\t-V 1440: "308+140" if 60fps available, "271+140" if not\n' +
+    '\t-V 2160: "315+140" if 60fps available, "313+140" if not')
+    .option('-F, --list-formats <boolean>', 'same as youtube-dl -F, list all available formats of video')
+    .option('-V, --vertical-resolution <number>', `vertical resolution, default is "720", available options: ${JSON.stringify(VALID_VERTICAL_RESOLUTION)}`)
+    .option('-D, --disable-socks5-proxy <boolean>', 'disable socks5 proxy, by default it is enabled')
     .option('-H, --socks5-host <string>', 'socks5 proxy host, default is "127.0.0.1"')
     .option('-P, --socks5-port <number>', 'socks5 proxy port, default is "1086"')
     .option('-A, --additional-options <string>', 'additional options, would be appended with built command directly, e.g: $builtCommand $additionalOptions')
     .parse(process.argv);
-console.log(program);
-console.log(1, program.socks5Host);
-console.log(1, program.additionalOptions);
-console.log(1, program.listFormats);
 let ARGS_SOURCE = program.source;
 let ARGS_OUTPUT_DIR = program.outputDir;
 let ARGS_OUTPUT_NAME = program.outputName;
-const ARGS_FORMAT = program.format;
+let ARGS_FORMAT = program.format;
 const ARGS_LIST_FORMATS = program.listFormats !== undefined;
 let ARGS_VERTICAL_RESOLUTION = program.verticalResolution;
+const ARGS_DISABLE_SOCKS5_PROXY = program.disableSocks5Proxy !== undefined;
 const ARGS_SOCKS5_HOST = program.socks5Host === undefined ? DEFAULT_SOCKS5_HOST : program.socks5Host;
 const ARGS_SOCKS5_PORT = program.socks5Port === undefined ? DEFAULT_SOCKS5_PORT : program.socks5Port;
 const ARGS_ADDITIONAL_OPTIONS = program.additionalOptions === undefined ? '' : program.additionalOptions;
@@ -66,15 +97,24 @@ class YoutubeDLQuick {
             console.log('Process validating ...');
             // validate youtube-dl installed or not
             if (!shell.which('youtube-dl')) {
-                console.log('Command "youtube-dl" not found, you need to install "youtube-dl" first.\nOSX e.g: brew install youtube-dl');
+                console.log('Command "youtube-dl" not found, you need to install it first.\n' +
+                    'e.g OSX: brew install youtube-dl');
                 process.exit(1);
             }
             // validate ARGS_URL
             if (ARGS_SOURCE === undefined) {
-                console.log('Option "url" required, please provide -u option!');
+                console.log('Option "source" required, please provide -s option!');
                 process.exit(1);
             }
-            if (!isUrl(ARGS_SOURCE) && (yield LibFs.stat(ARGS_SOURCE))) { // means a file
+            if (isUrl(ARGS_SOURCE) && ARGS_SOURCE.indexOf('list') !== -1) {
+                IS_SOURCE_PLAYLIST = true;
+                console.log('Download source is a playlist');
+            }
+            if (!isUrl(ARGS_SOURCE)
+                && (yield LibFs.exists(ARGS_SOURCE))
+                && (yield LibFs.stat(ARGS_SOURCE)).isFile()) { // means a file
+                IS_SOURCE_LISTFILE = true;
+                console.log('Download source is a list file');
             }
             // validate ARGS_OUTPUT_DIR
             if (ARGS_OUTPUT_DIR === undefined && LibOs.platform() === 'darwin') {
@@ -83,11 +123,6 @@ class YoutubeDLQuick {
             else if (ARGS_OUTPUT_DIR === undefined) {
                 console.log('Option "output dir" required, please provide -o option!');
                 process.exit(1);
-            }
-            // is playlist
-            if (ARGS_SOURCE.indexOf('list') !== -1) {
-                IS_SOURCE_PLAYLIST = true;
-                console.log('Target url is a playlist');
             }
             // validate ARGS_OUTPUT_NAME
             if (ARGS_OUTPUT_NAME === undefined && IS_SOURCE_PLAYLIST) {
@@ -100,14 +135,77 @@ class YoutubeDLQuick {
             if (ARGS_VERTICAL_RESOLUTION === undefined) {
                 ARGS_VERTICAL_RESOLUTION = DEFAULT_VERTICAL_RESOLUTION;
             }
-            else if (VALID_VERTICAL_RESOLUTION.indexOf(ARGS_VERTICAL_RESOLUTION) === -1) {
-                console.log(`Option "vertical resolution" is restrict to ${JSON.stringify(VALID_VERTICAL_RESOLUTION)}!`);
+            else if (ARGS_VERTICAL_RESOLUTION
+                && VALID_VERTICAL_RESOLUTION.indexOf(ARGS_VERTICAL_RESOLUTION) === -1) {
+                console.log(`Option "vertical resolution" is restrict to one of ${JSON.stringify(VALID_VERTICAL_RESOLUTION)}!`);
                 process.exit(1);
+            }
+            // validate ARGS_FORMAT
+            if (ARGS_FORMAT === undefined) {
+                ARGS_FORMAT = DEFAULT_FORMAT[ARGS_VERTICAL_RESOLUTION]['60'];
             }
         });
     }
     _process() {
         return __awaiter(this, void 0, void 0, function* () {
+            // prepare command base
+            let cmdBase = 'youtube-dl';
+            if (!ARGS_DISABLE_SOCKS5_PROXY) {
+                cmdBase += ` --proxy socks5://${ARGS_SOCKS5_HOST}:${ARGS_SOCKS5_PORT}`;
+            }
+            cmdBase += ` -o "${LibPath.join(ARGS_OUTPUT_DIR, ARGS_OUTPUT_NAME)}"`;
+            // list formats handled first
+            if (ARGS_LIST_FORMATS && isUrl(ARGS_SOURCE)) {
+                yield this._processListFormats(cmdBase);
+            }
+            yield this._executeWithErrorHandling(cmdBase, ARGS_SOURCE);
+        });
+    }
+    _processListFormats(cmdBase) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this._execute(`${cmdBase} -F`);
+        });
+    }
+    _executeWithErrorHandling(command, source) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const finalCommand = `${command} -f "${ARGS_FORMAT}" ${source}`;
+            try {
+                yield this._execute(finalCommand);
+            }
+            catch (e) {
+                if (e === ERROR_UNAVAILABLE_FORMAT) {
+                    // retry with downgrade format
+                    try {
+                        const retryCommand = `${command} -f "${DEFAULT_FORMAT[ARGS_VERTICAL_RESOLUTION]['30']}" ${source}`;
+                        console.log(`retry with command: ${retryCommand}`);
+                        yield this._execute(retryCommand);
+                    }
+                    catch (e) {
+                        // retry failed
+                        console.log(e);
+                    }
+                }
+                else {
+                    // unexpected error
+                    console.log(e);
+                }
+            }
+        });
+    }
+    _execute(command) {
+        console.log(`execute command: ${command}`);
+        return new Promise((resolve, reject) => {
+            const child = shell.exec(command, (code, stdout, stderr) => {
+                // unexpected error
+                if (code !== 0 && stderr.trim() !== ERROR_UNAVAILABLE_FORMAT) {
+                    return reject(`Error in "${command}"\ncode: ${code}, stderr: ${stderr}`);
+                }
+                // unavailable format error
+                if (code !== 0 && stderr.trim() === ERROR_UNAVAILABLE_FORMAT) {
+                    return reject(ERROR_UNAVAILABLE_FORMAT);
+                }
+                return resolve();
+            });
         });
     }
 }
